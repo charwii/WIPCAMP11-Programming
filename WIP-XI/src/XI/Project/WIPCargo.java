9e8d419875729e9edfbe52d898396d82bc76b247
charwii <51124823+charwii@users.noreply.github.com>
2019-05-31 11:27:39 +0700

    add project

--- WIP-XI/src/XI/Project/WIPCargo.java
+++ WIP-XI/src/XI/Project/WIPCargo.java
@@ -1,30 +1,75 @@
 /*
  * WIP XI Computer Programing 
  */
-
 package XI.Project;
+
+import java.util.Scanner;
 
 /**
  *
  * @author sittiwatlcp
  */
 public class WIPCargo {
+
     public static void main(String[] args) {
-        int age = 18;
-        String name = "Wippo";
-        double money = 1100.11;
-        
-        boolean online = true;
-        String status;
-        
-        System.out.println("Hi! my name is " + name + " i'm " + age + " Y/O ");
-        
-        if(online == true){
-            status = "Online";
-        } else {
-            status = "Offline";
+        int item = 5;
+        int limit = 10;
+        int Menu;
+
+        System.out.println("item" + item);
+        System.out.println("limit" + limit);
+
+        System.out.print("Menu Nakubb");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("I.Add");
+        System.out.println("II.Remove");
+        System.out.println("III.Check");
+        System.out.println("Please input Number");
+
+        Menu = sc.nextInt();
+
+        if (Menu == 1) {
+            System.out.println("I.Add");
+
+            if (item < limit) {
+                System.out.println("input amount to add");
+                int amount = sc.nextInt();
+                if (item + amount > limit) {
+                    System.out.println(" item เกิน ");
+                } else {
+                    item = item + amount;
+                    System.out.println("item" + item);
+                    System.out.println("limit" + limit);
+                }
+            } else {
+                System.out.println("It's Full");
+            }
+
         }
-        
-        System.out.println("I'm currently " + status + " and have " + money + "THB. left.");
+
+        if (Menu == 2) {
+            if (item > 0) {
+
+                System.out.println("remove");
+                System.out.println("input amount to remove");
+                int amount = sc.nextInt();
+                if (item - amount < 0) {
+                    System.out.println("remove mai dai");
+                } else {
+                    item = item - amount;
+                    System.out.println("item" + item);
+                    System.out.println("limit" + limit);
+                }
+
+            } else {
+                System.out.println("It's Emthy");
+            }
+        }
+        if (Menu == 3) {
+            System.out.println("item" + item);
+            System.out.println("limit" + limit);
+        }
     }
 }
